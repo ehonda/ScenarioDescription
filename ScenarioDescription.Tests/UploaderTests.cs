946ab3dae0dcc1af0e906d7ec86b77b9c840377d
Dennis Renz <dennis@r-renz.de>
2022-12-13 20:14:09 +0100

    POC: Add description to scenario

---
+++ ScenarioDescription.Tests/UploaderTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+
+namespace ScenarioDescription.Tests;
+
+public class UploaderTests
+{
+    public static class Data
+    {
+        public record Scenario(IReadOnlyList<string> Skus, string? Description = null)
+        {
+            public override string ToString() => Description ?? base.ToString()!;
+        }
+
+        public static IEnumerable<Scenario> WithInvalidSkus => new Scenario[]
+        {
+            new(new List<string> { "invalid" }, "One invalid value"),
+            new(new List<string> { "valid", "invalid" }, "One invalid, one valid value")
+        };
+    }
+
+    [Test]
+    [Description("TEST_CASE_DESCRIPTION")]
+    public void Upload_works([ValueSource(typeof(Data), nameof(Data.WithInvalidSkus))] Data.Scenario scenario)
+    {
+        // Arrange
+        
+        // Act
+
+        // Assert
+        true.Should().BeFalse();
+    }
+}
