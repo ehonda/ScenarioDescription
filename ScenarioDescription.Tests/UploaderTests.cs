508d7f84befa0819245421796fa6939bb18c41d8
Dennis Renz <dennis@r-renz.de>
2022-12-13 20:22:00 +0100

    Add nice output in non described case

--- ScenarioDescription.Tests/UploaderTests.cs
+++ ScenarioDescription.Tests/UploaderTests.cs
@@ -6,27 +6,32 @@ public class UploaderTests
 {
     public static class Data
     {
-        public record Scenario(IReadOnlyList<string> Skus, string? Description = null)
+        public record Scenario(IReadOnlyList<string> Skus);
+        
+        public record ScenarioWithDescription(IReadOnlyList<string> Skus, string? Description = null) : Scenario(Skus)
         {
-            public override string ToString() => Description ?? base.ToString()!;
+            // TODO: Don't print empty Description in fallback case
+            public override string ToString() => Description ?? base.ToString();
         }
 
-        public static IEnumerable<Scenario> WithInvalidSkus => new Scenario[]
+        public static IEnumerable<ScenarioWithDescription> WithInvalidSkus => new ScenarioWithDescription[]
         {
             new(new List<string> { "invalid" }, "One invalid value"),
-            new(new List<string> { "valid", "invalid" }, "One invalid, one valid value")
+            new(new List<string> { "valid", "invalid" }, "One invalid, one valid value"),
+            new(new List<string> { "invalid", "valid", "invalid" }),
         };
     }
 
     [Test]
     [Description("TEST_CASE_DESCRIPTION")]
-    public void Upload_works([ValueSource(typeof(Data), nameof(Data.WithInvalidSkus))] Data.Scenario scenario)
+    public void Upload_works([ValueSource(typeof(Data), nameof(Data.WithInvalidSkus))]
+        Data.ScenarioWithDescription scenarioWithDescription)
     {
         // Arrange
         
         // Act
 
         // Assert
-        true.Should().BeFalse();
+        true.Should().BeTrue();
     }
 }
